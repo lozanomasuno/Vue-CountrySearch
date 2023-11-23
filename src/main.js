74f3d70c8df43b1b43f0157bdfd6f5c785efec2d
lozanomasuno <lozanomasuno@gmail.com>
2023-11-23 12:05:22 -0500

    apollo and grapql

--- src/main.js
+++ src/main.js
@@ -3,5 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.css'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
 
+const httplink = createHttpLink({
+    uri: "https://countries.trevorblades.com/"
+})
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+    link: httplink,
+    cache,
+})
 createApp(App).use(store).use(router).mount('#app')
